0402f23aae30b59ae77d084160a4271e62dd1e0b
Paula-021 <anapaula_solovecadugabriel@hotmail.com>
2025-02-17 15:07:18 -0300

    subindo projeto

---
+++ src/cadastropessoas/models/PessoaJuridicaModel.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cadastropessoas.models;
+
+import cadastropessoas.daos.PessoaFisicaDao;
+import cadastropessoas.daos.PessoaJuridicaDao;
+import cadastropessoas.entities.PessoaFisica;
+import cadastropessoas.entities.PessoaJuridica;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author anapa
+ */
+public class PessoaJuridicaModel {
+    
+    public void insert(PessoaJuridica pj) throws SQLException{
+        PessoaJuridicaDao dao = new PessoaJuridicaDao();
+        dao.insert(pj);
+    }
+
+    public ArrayList<PessoaJuridica> selectAll() throws SQLException {
+        PessoaJuridicaDao dao = new PessoaJuridicaDao();
+        ArrayList<PessoaJuridica>pessoasJuridicas = dao.selectAll();
+        return pessoasJuridicas;
+    }
+
+    public void update(PessoaJuridica pj) throws SQLException {
+        PessoaJuridicaDao dao = new PessoaJuridicaDao();
+        dao.update(pj);
+    }
+
+    public void delete(int id) throws SQLException {
+        PessoaJuridicaDao dao = new PessoaJuridicaDao();
+        dao.delete(id);
+    }
+    
+}
